339f2ccd10a966a1375f2c98479c9587bf443bf6
smogielnicki <mogies@rpi.edu>
2016-12-10 17:00:53 -0500

    Update iplc-sim-push-pipeline.c

--- iplc-sim-push-pipeline.c
+++ iplc-sim-push-pipeline.c
@@ -404,6 +404,8 @@ void iplc_sim_push_pipeline_stage()
                 inserted_nop += 1;
             }
         }
+     
+     pipeline_cycles += 1;
     }
     
     /* 4. Check for SW mem acess and data miss .. add delay cycles if needed */
